256d544c3ed3e4aec8b368727999d9dac9654f7e
Emerson Francisco <emersonairton@hotmail.com>
2022-02-22 20:49:07 -0300

    update main.go folder pacotes/reuso

--- pacotes/reuso/main.go
+++ pacotes/reuso/main.go
@@ -6,5 +6,9 @@ import (
 )
 
 func main() {
-	fmt.Println(area.Circ(6.0))
+	fmt.Printf("a Area do circulo é : %.2fr²\n", area.Circ(6.0))
+	fmt.Printf("a Area do Retangulo é: %.2fm²\n", area.Rect(2, 8))
+	fmt.Printf("a Area do Quadrado é: %.2fm²\n", area.Quad(3))
+	fmt.Printf("a Area do Cubo é: %.2fm³\n", area.Cubo(3, 3, 4))
+	fmt.Printf("a Area do Triangulo Equilátero é: %.2fm²", area.TrianguloEq(3, 3))
 }
